7d50e18e7f0fc09b53b71cfce664068148afd741
David Muir <1102119m@student.gla.ac.uk>
2014-07-22 23:04:23 +0100

    Updated - procedures altered and partially tested. Initial results are correct. A start has been made on the All_Analysis function.

--- Data_Set_Test.cpp
+++ Data_Set_Test.cpp
@@ -2,35 +2,49 @@
 
 int main()
 {
+  const char* Name;
+
+  /****************************************************************************/
   // Manual input.
+  /****************************************************************************/
+
   Data_Set Numbers(1, 7);
-  double** Data = Numbers.Input_Data_Set("Manual");
-  double Mean = Numbers.Mean(Data, 0);
-  double Median = Numbers.Median(Data, 0);
-  double Random_Error = Numbers.Random_Uncertainty(Data, 0);
-  double Mode = Numbers.Mode(Data, 0);
-  Data_Set Numbers_Subset = Numbers.Data_Subset(Data, 0, 3, 6);
-  double** Subset = Numbers.Input_Data_Subset(Data, 0, 3.0, 6.0);
-  cout << Mean << endl;
-  cout << Median << endl;
-  cout << Random_Error << endl;
-  cout << Mode << endl;
+  double** Data = Numbers.Input_Data_Set("Manual", "Nothing");
+  double Mean = Numbers.Mean(Data, 1);
+  double Median = Numbers.Median(Data, 1);
+  double Random_Error = Numbers.Random_Uncertainty(Data, 1);
+  double Mode = Numbers.Mode(Data, 1);
+  Data_Set Numbers_Subset = Numbers.Data_Subset(Data, 1, 3, 6);
+  double** Subset = Numbers.Input_Data_Subset(Data, 1, 3.0, 6.0);
 
-  Mean = Numbers_Subset.Mean(Subset, 0);
-  cout << Mean << endl;
+  cout << "Manual Input" << endl;
+  cout << "Mean: " << Mean << endl;
+  cout << "Median: " << Median << endl;
+  cout << "Random Uncertainty: " << Random_Error << endl;
+  cout << "Mode: " << Mode << endl;
 
+  Mean = Numbers_Subset.Mean(Subset, 1);
+  cout << "Mean Subset: " << Mean << endl;
+
+  Name = "Numbers.txt";
+  Numbers.All_Analysis(Numbers, Data, 1, Name);
+  
+  /****************************************************************************/
   // File input.
-/*
-  cout << "File information:" << endl;
+  /****************************************************************************/
+  
+  Data_Set Numbers_File(4, 7);
+  double** Data_2 = Numbers_File.Input_Data_Set("File", "File_Input.txt");
+  Mean = Numbers_File.Mean(Data_2, 4);
+  Median = Numbers_File.Median(Data_2, 4);
+  Random_Error = Numbers_File.Random_Uncertainty(Data_2, 4);
+  // Mode = Numbers_File.Mode(Data_2, 1);
 
-  Data_Set Numbers_File(1, 7);
-  double** Data_2 = Numbers_File.Input_Data_Set("File");
-  Mean = Numbers_File.Mean(Data_2, 0);
-  Median = Numbers_File.Median(Data_2, 0);
-  Random_Error = Numbers_File.Random_Uncertainty(Data_2, 0);
-  cout << Mean << endl;
-  cout << Median << endl;
-  cout << Random_Error << endl;*/
+  cout << "File Input" << endl;
+  cout << "Mean: " << Mean << endl;
+  cout << "Median: " << Median << endl;
+  cout << "Random Uncertainty: " << Random_Error << endl;
+  //cout << "Mode: " << Mode << endl;
 
   // Array input.
 
