6a4b59a91bc3a3ae7067a6943633bc37a6d73e34
David Muir <1102119m@student.gla.ac.uk>
2014-07-27 20:11:05 +0100

    Data set updated with population and sample variances and standard deviations.

--- Data_Set_Test.cpp
+++ Data_Set_Test.cpp
@@ -16,12 +16,20 @@ int main()
   double Mode = Numbers.Mode(Data, 1);
   Data_Set Numbers_Subset = Numbers.Data_Subset(Data, 1, 3, 6);
   double** Subset = Numbers.Input_Data_Subset(Data, 1, 3.0, 6.0);
+  double Population_Variance = Numbers.Population_Variance(Numbers, Data, 1);
+  double Sample_Variance = Numbers.Sample_Variance(Numbers, Data, 1);
+  double Population_Standard_Deviation = Numbers.Population_Standard_Deviation(Numbers, Data, 1);
+  double Sample_Standard_Deviation = Numbers.Sample_Standard_Deviation(Numbers, Data, 1);
 
   cout << "Manual Input" << endl;
   cout << "Mean: " << Mean << endl;
   cout << "Median: " << Median << endl;
   cout << "Random Uncertainty: " << Random_Error << endl;
   cout << "Mode: " << Mode << endl;
+  cout << "Population Variance: " << Population_Variance << endl;
+  cout << "Population Standard Deviation: " << Population_Standard_Deviation << endl;
+  cout << "Sample Variance: " << Sample_Variance << endl;
+  cout << "Sample Standard Deviation: " << Sample_Standard_Deviation << endl;
 
   Mean = Numbers_Subset.Mean(Subset, 1);
   cout << "Mean Subset: " << Mean << endl;
